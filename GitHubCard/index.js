1a2b09a30a16a9f94b96de139bd07344e6cfb0c0
vamendez00 <veromendez@gmail.com>
2021-03-21 19:12:35 -0500

    in-progress submission

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,26 @@
+import axios from 'axios';
+
+// console.log("CHECK OUT AXIOS: \n \n", axios);
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
-    https://api.github.com/users/<your name>
+    https://api.github.com/users/vamendez00
 */
-//arbitrary change for submission
+const myProfile = axios.get("https://api.github.com/users/vamendez00")
+.then (dataResponse => {
+  console.log('myData:', dataResponse);
+  const myInfo = dataResponse.data;
+  console.log('UserInfo:', myInfo);
+  return myInfo;
+})
+.catch(error => {
+  console.log("The data was not returned", error);
+})
+.finally(() => {
+  console.log("DONE!");
+});
+
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -11,11 +28,67 @@
 
     Skip to STEP 3.
 */
+function cardMaker (){
+  
+  const newCard = document.createElement("div");
+  newCard.classList.add("card");
+
+  const newImage = document.createElement("img");
+  // newImage.src = avatar_url;
+  newCard.appendChild(newImage);
+
+  const cardInfo = document.createElement("div");
+  cardInfo.classList.add("card-info");
+  newCard.appendChild(cardInfo);
+
+  const fullName = document.createElement("h3");
+  fullName.textContent = `Name: `;
+  fullName.classList.add("name");
+  cardInfo.appendChild(fullName);
+
+  const userName = document.createElement("p");
+  userName.textContent = `UserName: `;
+  userName.classList.add("username");
+  cardInfo.appendChild(userName);
+  
+  const profileLocation = document.createElement("p");
+  profileLocation.textContent = "Location:";
+  cardInfo.appendChild(profileLocation);
+
+  const profile = document.createElement("p");
+  profile.textContent = "Profile: ";
+  cardInfo.appendChild(profile);
+
+  const profileLink = document.createElement("a");
+  profileLink.href = `GitHub link: `;
+  profile.appendChild(profileLink);
+    
+  const userFollowers = document.createElement("p");
+  userFollowers.textContent = `Followers: `;
+  cardInfo.appendChild(userFollowers);
+
+  const userFollowing = document.createElement("p");
+  userFollowing.textContent = `Following: `;
+  cardInfo.appendChild(userFollowing);
+
+  const userBio = document.createElement("p");
+  userBio.textContent = `Bio:`;
+  cardInfo.appendChild(userBio);
+
+  console.log(newCard);
+}
+
 
 /*
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
+
+const cards = document.querySelector(".cards");
+const userCardInfo = cardMaker(myProfile);
+cards.appendChild(userCardInfo);
+
+
 
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
